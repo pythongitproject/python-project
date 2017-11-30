8f943013040a8604b45947c9a48b8df37b06dbc2
linweili <295287765@qq.com>
2017-11-30 22:33:03 +0800

    quanzhan

---
+++ quanzhan/web.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+#coding==utf-8
+from wsgiref.simple_server import make_server
+
+urls = {
+    '/new':'path: /new',
+    '/sys':'path: /sys'
+}
+
+def run_server(environ, start_response):
+    start_response('200 ok',[('Content-Type','text/html')])
+    url = environ['PATH_INFO']
+    if url in urls.keys():
+        ret = urls[url]
+    else:
+        ret = '404'
+    return [ret.encode('utf-8')]
+
+if __name__ == '__main__':
+    httpd = make_server('',8000,run_server)
+    httpd.serve_forever()
