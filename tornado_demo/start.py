ed80cece1ec61a6bb3fa073933896bd7ed1971a8
linweili <295287765@qq.com>
2017-12-02 23:58:29 +0800

    未登录发布

--- tornado_demo/start.py
+++ tornado_demo/start.py
@@ -8,6 +8,12 @@ import tornado_demo.uimodule as md
 
 
 INPUT_LIST = []
+USER_INFO = {
+    'is_login':None,
+}
+ITEM_LIST = [
+    {'title':'python基础','content':'数据类型、条件判断、切片'},
+]
 class MainHandler(tornado.web.RequestHandler):
     def get(self, *args, **kwargs):
         # self.write('hello, get')
@@ -21,6 +27,29 @@ class MainHandler(tornado.web.RequestHandler):
         # self.write('hello, post',xxxooo = INPUT_LIST)
         self.render('s1.html',npm = 'NPM888', xxxooo=INPUT_LIST)
 
+class IndexHandler(tornado.web.RequestHandler):
+    def get(self, *args, **kwargs):
+        # self.render('index.html',is_login = IS_LOGIN['is_login'])
+        self.render('index.html',user_info = USER_INFO,item_list = ITEM_LIST)
+
+class LoginHandler(tornado.web.RequestHandler):
+    def post(self, *args, **kwargs):
+        username = self.get_argument('username',None)
+        pwd = self.get_argument('pwd',None)
+        if username == 'linweili' and pwd == '123':
+            USER_INFO['is_login'] = True
+            USER_INFO['username'] = username
+        self.redirect('/index')
+
+class PublishHandler(tornado.web.RequestHandler):
+    def post(self, *args, **kwargs):
+        if USER_INFO['is_login']:
+            title = self.get_argument('title', None)
+            content = self.get_argument('content', None)
+            temp = {'title': title, 'content': content}
+            ITEM_LIST.append(temp)
+        # self.render('index.html',user_info = USER_INFO)
+        self.redirect('/index')
 
 settings = {
     'template_path':'template',    #模板路径配置
@@ -31,7 +60,10 @@ settings = {
 }
 #路由映射、路由系统
 application = tornado.web.Application([
-    (r'/index',MainHandler),
+    # (r'/index',MainHandler),
+    (r'/index',IndexHandler),
+    (r'/login',LoginHandler),
+    (r'/pub',PublishHandler),
 ], **settings)
 
 if __name__ == '__main__':
