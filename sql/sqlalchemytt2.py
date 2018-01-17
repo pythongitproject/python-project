35858f76942f8b420f92f9b3766a7850c8ce6bd8
linweili <295287765@qq.com>
2018-01-17 15:41:37 +0800

    多对多 v2.1

--- sql/sqlalchemytt2.py
+++ sql/sqlalchemytt2.py
@@ -32,19 +32,56 @@ class Women(Base):
     bf = relationship("Men",secondary = Men_to_Women.__table__)
 
 # Base.metadata.create_all(engine)
+
+#增
 # m1 = Men(name = 'lb')
 # w1 = Women(name = 'wuq')
 # w2 = Women(name = 'wuq1')
 # session.add_all([m1,w1,w2])
-
+#直接插入id
 # m1id = session.query(Men.id).filter_by(id='1')
 # w1id = session.query(Women.id).filter_by(id='1')
 # mw1 = Men_to_Women(men_id=m1id,women_id=w1id)
 # session.add(mw1)
 
+#针对一个
 # m1 = session.query(Men).filter_by(id='2').first()
 # w1 = session.query(Women).filter_by(id='1').all()
 # m1.gf=w1
 # session.add(m1)
 
-session.commit()
+#多对多切片
+# m1 = session.query(Men).all()
+# w1 = session.query(Women).filter_by(name='wuq1').first()
+# w1.bf=m1[2:5]
+# session.add(w1)
+
+#删除
+# s = session.query(Men).filter_by(id='1').first()
+# session.delete(s)
+# session.delete(session.query(Men).filter(Men.id=='2').one())
+
+#更新
+# session.query(Men).filter_by(id='3').update({'name':'liuliuliu'})
+# session.query(Men).filter_by(id='3').update({Men.name:Men.name+'666'},synchronize_session = False)
+
+#连表查询
+# ret1 = session.query(Men, Men_to_Women).filter(Men.id == Men_to_Women.men_id).all()   #where 关联
+# print(ret1)
+# ret2 = session.query(Men).join(Men_to_Women).all()   #inner join
+# print(ret2)
+# ret3 = session.query(Men).join(Men_to_Women, isouter=True).all()   #outer join
+# print(ret3)
+
+# 组合查询
+q1 = session.query(Men.name).filter(Men.id > 5)
+q2 = session.query(Men.name).filter(Men.id < 4)
+ret = q1.union(q2).all()                        #union select
+print(ret)
+
+q1 = session.query(Men.name).filter(Men.id > 5)
+q2 = session.query(Men.name).filter(Men.id < 4)
+ret1 = q1.union_all(q2).all()                  #union all
+print(ret1)
+# session.commit()
+session.close()
