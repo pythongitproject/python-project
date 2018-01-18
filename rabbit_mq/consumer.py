318266ecdfeeb98c1fdfb6a942514ad7de2896a8
linweili <295287765@qq.com>
2018-01-18 09:55:00 +0800

    mq_normal v1

--- rabbit_mq/consumer.py
+++ rabbit_mq/consumer.py
@@ -8,13 +8,23 @@ connection = pika.BlockingConnection(pika.ConnectionParameters(
     host='localhost'
 ))
 channel = connection.channel()
-channel.queue_declare(queue='hello')
+# make message persistent
+# channel.queue_declare(queue='hello',durable=True)
+
+try:
+    channel.queue_declare(queue='hello', durable=True)
+except:
+    channel = connection.channel()
+    channel.queue_delete(queue='hello')
+    channel.queue_declare(queue='hello', durable=True)
 
 def callback(ch, method, properties, body):
     print("Received %r" % body)
+    ch.basic_ack(delivery_tag=method.delivery_tag)
 
 channel.basic_consume(callback,
-                      queue='hello',no_ack=True
+                      queue='hello',
+                      no_ack=False
                       )
 print('Waiting for messages. To exit press CTRL+C')
 channel.start_consuming()
--- rabbit_mq/producer.py
+++ rabbit_mq/producer.py
@@ -8,12 +8,24 @@ connection = pika.BlockingConnection(pika.ConnectionParameters(
     host='localhost'
 ))
 channel = connection.channel()
-channel.queue_declare(queue='hello')
+#durable   消息不丢失
+# channel.queue_declare(queue='hello', durable=True)
+
+try:
+    channel.queue_declare(queue='hello', durable=True)
+except:
+    channel = connection.channel()
+    channel.queue_delete(queue='hello')
+    channel.queue_declare(queue='hello', durable=True)
+
 
 channel.basic_publish(
     exchange='',
     routing_key='hello',
-    body='hello kugou'
+    body='hello kugou',
+    properties=pika.BasicProperties(
+        delivery_mode=2   # make message persistent
+    )
 )
 print('producer: sent hello world')
 connection.close()
