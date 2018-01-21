b2ca6e03d2755e4a519d4f5c1d78d1783b4b5f2d
linweili <295287765@qq.com>
2018-01-21 19:33:37 +0800

    login.js

--- tornado_drawer/statics/js/login.js
+++ tornado_drawer/statics/js/login.js
@@ -1,4 +1,16 @@
 
+    function isPoneAvailable(str) {
+          var myreg=/^[1][3,4,5,,6,7,8,9][0-9]{9}$/;
+          if (!myreg.test(str)) {
+              return true;
+          } else {
+              return false;
+          }
+      }
+    //刷新验证码
+    function Changecode() {
+            $("#getcode").attr("src","/check_code?flag="+Math.random());
+    }
     $("#sb").on("click",function () {
         var flag = true;
             if($("#telno").val().trim()==''){
@@ -64,7 +76,4 @@
 
     });
 
-    //刷新验证码
-    function Changecode() {
-            $("#getcode").attr("src","/check_code?flag="+Math.random());
-    }
+
